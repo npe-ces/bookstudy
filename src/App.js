3267a38a07ab2a72dd7d7381309f81f410e87408
npe-ces <43264952+npe-ces@users.noreply.github.com>
2018-10-28 23:04:29 +0900

    책 따라침
    todo : webpack 공부

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom'
+import { Home, About, Posts } from 'pages';
+import Menu from 'components/Menu';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Menu/>
+      {/* exact를 넣으면 반드시 그 경로여야만 넘어감 */}
+      <Route exact path="/" component={Home} />
+      {/*<Route exact path="/about" component={About} /> */}
+      {/* param을 선택적으로 받는다. */}
+      <Route path="/about/:name?" component={About} />
+      <Route path="/posts" component={Posts} />
+    </div>
+  );
+};
 
 export default App;
