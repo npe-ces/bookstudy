27d7cd2c016f5be1064f1ad1ba83326a5d845444
npe-ces <43264952+npe-ces@users.noreply.github.com>
2018-10-29 23:20:00 +0900

    의존성 추가 및 Chunk에대한 코드 추가함.
    이거저거 공부하면서 많이 써봤다.. 의존성쪽은 웹팩 버전 업데이트 되면서 수정 많이된것같으니 웹팩 공부할것

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 import { Home, About, Posts } from 'pages';
 import Menu from 'components/Menu';
+import AsyncSplitMe from 'components/AsyncSplitMe';
 
 const App = () => {
   return (
     <div>
       <Menu/>
+      <AsyncSplitMe/>
       {/* exact를 넣으면 반드시 그 경로여야만 넘어감 */}
       <Route exact path="/" component={Home} />
       {/*<Route exact path="/about" component={About} /> */}
