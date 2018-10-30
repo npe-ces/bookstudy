f9b4511e4a05c88b4cb591c2d5eeab332a34a9c0
npe-ces <43264952+npe-ces@users.noreply.github.com>
2018-10-30 23:28:00 +0900

    백엔드단 수정

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,9 +1,13 @@
 const Router = require('koa-router');
+const posts = require('../posts');
 
 const api = new Router();
 
-api.get('/test', (ctx)=>{
-    ctx.body = 'test 성공!';
-});
+// api.get('/test', (ctx)=>{
+//     ctx.body = 'test 성공!';
+// });
+
+
+api.use('/posts',posts.routes());
 
 module.exports = api;
--- blog/blog-backend/src/posts/index.js
+++ blog/blog-backend/src/posts/index.js
@@ -2,11 +2,11 @@ const Router = require('koa-router');
 
 const posts = new Router();
 
-const printInfo = (e) => {
-    e.body = {
-        method: e.method,
-        path: e.path,
-        params: e.params,
+const printInfo = (ctx) => {
+    ctx.body = {
+        method: ctx.method,
+        path: ctx.path,
+        params: ctx.params
     };
 };
 
