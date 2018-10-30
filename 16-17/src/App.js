e2adcc7e3058f55b5a6726e2db291b0adb0002c2
npe-ces <43264952+npe-ces@users.noreply.github.com>
2018-10-30 16:40:46 +0900

    폴더이동 및 신규프로젝트생성

---
+++ 16-17/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route } from 'react-router-dom'
+import { Home, About, Posts } from 'pages/index.async.js';
+import Menu from 'components/Menu';
+
+const App = () => {
+  return (
+    <div>
+      <Menu/>
+      {/* exact를 넣으면 반드시 그 경로여야만 넘어감 */}
+      <Route exact path="/" component={Home} />
+      {/*<Route exact path="/about" component={About} /> */}
+      {/* param을 선택적으로 받는다. */}
+      <Route path="/about/:name?" component={About} />
+      <Route path="/posts" component={Posts} />
+    </div>
+  );
+};
+
+export default App;
